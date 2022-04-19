ecb121f0008ffe4ee7e7930fc1064570f0870eb2
Rhea <rheaazarraga@gmail.com>
2022-04-18 17:35:18 -0700

    change all get routes to follow path - url rule, and remove redundant get route to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  # This new pattern routes a GET request for the URL /help to the help action in the Static Pages controller. As with the rule for the root route, this creates two named routes, help_path and help_url
-  get '/help', to: 'static_pages#help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
-
-#test for github commits
