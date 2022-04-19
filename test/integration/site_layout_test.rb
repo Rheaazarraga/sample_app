6434c23bb4895ace8a59c572deedcd36d9b624c5
Rhea <rheaazarraga@gmail.com>
2022-04-18 17:49:34 -0700

    add assertion tests for all paths in app

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # allows us to test for the presence of a particular link–URL combination by specifying the tag name a and attribute href
+  
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
 end
