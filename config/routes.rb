a37232fadf4e02be458050c4754fbb5040749f22
Rhea <rheaazarraga@gmail.com>
2022-04-18 17:25:10 -0700

    change get route for help to create a help path to the help page url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
-  get 'static_pages/help'
+  # This new pattern routes a GET request for the URL /help to the help action in the Static Pages controller. As with the rule for the root route, this creates two named routes, help_path and help_url
+  get '/help', to: 'static_pages#help'
   get 'static_pages/about'
   get 'static_pages/contact'
 end
