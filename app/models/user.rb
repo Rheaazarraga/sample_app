0286870e5a159956a6a97ea6939e727a055824e4
Rhea <rheaazarraga@gmail.com>
2022-05-04 19:30:20 -0700

    add validation for email format with regular expression (or regex)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
 
   validates :email, presence: true, length: { maximum: 255 }
+
+  validate :email, format: { with: /<regular expression> / }
 end
