c6c15a3b2197aaec476019b062d8a5b99945c7d4
Rhea <rheaazarraga@gmail.com>
2022-05-04 19:37:27 -0700

    update validation, assign regex as a constant, ensuring that only email addresses that match the pattern will be considered valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :email, presence: true, length: { maximum: 255 }
 
-  validate :email, format: { with: /<regular expression> / }
+  validate :email, format: { with: VALID_EMAIL_REGEX }
 end
