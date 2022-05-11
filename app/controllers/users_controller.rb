e2c1365efbbb2e594d59876f257f141b3b6b2b23
Rhea <rheaazarraga@gmail.com>
2022-05-10 20:50:49 -0700

    correct conditional statement to an instance of a new user being created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if user.save
+    if @user.save
       # handle successful save
     else
     render 'new'
