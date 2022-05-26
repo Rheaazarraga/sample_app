d9735110143e7cfa173b9f48dec4cdbc3ed90824
Rhea <rheaazarraga@gmail.com>
2022-05-25 20:55:19 -0700

    define logged_in method for if a user is logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,9 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+
+  # returns true if the user is logged in, otherwise false
+  def logged_in?
+    !current_user.nil?
+  end
 end
