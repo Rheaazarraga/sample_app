12204c6c6931375c02ea4b9d4fcf6d336f63967f
Rhea <rheaazarraga@gmail.com>
2022-06-01 20:27:03 -0700

    update current_user for persistent sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,8 +14,13 @@ module SessionsHelper
 
   # returns the current logged-in user (if any)
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: :user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
     end
   end
 
