0ae57dcaf1bbdb1de5623fb947637511b4ea398f
Rhea <rheaazarraga@gmail.com>
2022-06-01 20:13:24 -0700

    implement logging in and remembering a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user &.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       reset_session
+      remember user
       log_in user
       redirect_to user
-      # log the user in and redirect to the user's show page
     else
-      # create an error msg
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
