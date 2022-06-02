0f02a5be54b0e03e77e9063feef7a99ca116c68a
Rhea <rheaazarraga@gmail.com>
2022-06-02 09:53:55 -0700

    fix typo in style rules, remove extra create method causing errors, format and lint files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    def create
-      @user = User.find_by(email: params[:session][:email].downcase)
-      if @user && @user.authenticate(params[:session][:password])
-        reset_session
-        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-        log_in @user
-        redirect_to @user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      reset_session
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
