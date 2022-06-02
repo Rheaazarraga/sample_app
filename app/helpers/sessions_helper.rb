74ac32ae1c5f66fdb82c12f5f7a6656dd2925be6
Rhea <rheaazarraga@gmail.com>
2022-06-01 20:20:16 -0700

    implement remembering the user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # returns the current logged-in user (if any)
   def current_user
     if session[:user_id]
