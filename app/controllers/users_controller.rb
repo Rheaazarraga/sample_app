4e1b8fbfe137b0a75d0f00c702401ce5a858cc22
Rhea <rheaazarraga@gmail.com>
2022-06-08 18:49:59 -0700

    add a before filter to protect the edit/update pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -49,6 +50,12 @@ class UsersController < ApplicationController
       unless logged_in?
       flash[:danger] = 'Please log in'
       redirect_to login_url
+    end
+
+    # confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
   end
 end
