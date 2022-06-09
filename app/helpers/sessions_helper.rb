8d228a4db0d0bf9e15d5865c9c220f2e8492d180
Rhea <rheaazarraga@gmail.com>
2022-06-08 19:14:21 -0700

    add code to store the forwarding URL

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -67,4 +67,9 @@ module SessionsHelper
   def current_user?(user)
     user && user == current_user
   end
+
+  # stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
