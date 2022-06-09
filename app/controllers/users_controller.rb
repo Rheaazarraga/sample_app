115b99707b69c9c4ed7a56e202517c50aa73b01c
Rhea <rheaazarraga@gmail.com>
2022-06-08 18:06:11 -0700

    fix syntax in logged_in_user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,9 @@ class UsersController < ApplicationController
 
     # confirms a logged-in user
     def logged_in_user
+      unless logged_in?
       flash[:danger] = 'Please log in'
       redirect_to login_url
     end
+  end
 end
